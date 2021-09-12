b808118c9bc1e30f012bc959fec5e2dbfd447988
anji <anji123@gmail.com>
2021-09-12 04:32:05 +0000

    2nd commit

--- src/main/java/jenkins-project1/App.java
+++ src/main/java/jenkins-project1/App.java
@@ -1,13 +1,15 @@
 package jenkins-project1;
+public class App {
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
     }
+  }
 }
+
